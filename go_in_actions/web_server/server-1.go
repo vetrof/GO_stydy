d8227889b41dceed761cc8b902f52e8483985cc9
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-06-11 22:04:45 +0500

    go server

---
+++ go_in_actions/web_server/server-1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main(){
+	http.HandleFunc("/hello", hello)
+	//http.HandleFunc("/goodbye/", goodbye)
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(res http.ResponseWriter, req *http.Request){
+	query := req.URL.Query()
+	name := query.Get("name")
+	age := query.Get("age")
+	if name == ""{
+		name = "indigo Montoya"
+	}
+	fmt.Fprintf(res, "Hello my name %s is %s", name, age)
+}
+
+func homePage(res http.ResponseWriter, req *http.Request){
+	if req.URL.Path != "/"{
+		http.NotFound(res, req)
+		return
+	}
+	fmt.Fprint(res, "HomePage")
+}
