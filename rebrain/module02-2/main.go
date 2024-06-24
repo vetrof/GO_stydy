294cf1695622c873c760cada6e11d2c045e7cd92
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-06-24 23:45:46 +0500

    module4

--- rebrain/module02-2/main.go
+++ rebrain/module02-2/main.go
@@ -1,21 +1,5 @@
 package main
 
-import "fmt"
-
-type AmericanVelocity float64
-type EuropeanVelocity float64
-
 func main() {
 
-	speedMS := 120.4
-	speedKmH := (speedMS / 1000) * 3600
-
-	speedMS2 := 130.0
-	speedMH := speedMS2 * 2.237
-
-	var EU EuropeanVelocity = EuropeanVelocity(speedKmH)
-	var AM AmericanVelocity = AmericanVelocity(speedMH)
-
-	fmt.Println(EU)
-	fmt.Println(AM)
 }
