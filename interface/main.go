2274c963804b8afb486a93df8d164397bb3abc29
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-06-30 19:33:20 +0500

    update

---
+++ interface/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+)
+
+type Employers struct {
+	Name string
+	Id   int
+}
+
+func (e *Employers) PrintEmployersInfo() {
+	fmt.Println(e.Id, e.Name)
+}
+
+func (e *Employers) Hash() string {
+	str := e.Name + strconv.Itoa(e.Id)
+	hash := sha256.New()
+	hash.Write([]byte(str))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	return hashString
+}
+
+func main() {
+	e := Employers{
+		Name: "assa",
+		Id:   99,
+	}
+	e.PrintEmployersInfo()
+	hash := e.Hash()
+	fmt.Println(e, hash)
+}
