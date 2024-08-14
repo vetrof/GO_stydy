227e229688154f24647ddb9e1f56aeb7b4dd6b11
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-08-14 23:05:35 +0500

    add http server

---
+++ golang-ninja/http server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name,omitempty"`
+}
+
+var (
+	users = []User{{1, "Vasya"}, {2, "Petya"}}
+)
+
+func main() {
+	http.HandleFunc("/users", handleUsers)
+
+	fmt.Println("start server")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleUsers(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUsers(w, r)
+	case http.MethodPost:
+		getUsers(w, r)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+	}
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	resp, err := json.Marshal(users)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Write(resp)
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	//todo
+}
