4ce2f5360db5abfda6eadf7cc8ca09d8de41292e
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-10-21 08:47:49 +0500

    add slice

--- temp.go
+++ temp.go
@@ -3,15 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	for n := 0; n >= 0; n++ {
+	for {
 
-		fmt.Println(n)
+		fmt.Println("qqqq")
 
-		if n > 1000 {
-			break
-		}
 	}
-	fmt.Println("one print")
-	
-	fmt.Println("two print")
 }
