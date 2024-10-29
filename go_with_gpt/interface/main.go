39299c0be013f16b7973fc4f67490dc290edfa7d
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-10-29 09:14:39 +0500

    add slice

--- go_with_gpt/interface/main.go
+++ go_with_gpt/interface/main.go
@@ -8,7 +8,7 @@ type UserActions interface {
 }
 
 type Greeter interface {
-	Greet()
+	UserActions
 }
 
 type User struct {
@@ -29,6 +29,16 @@ func (u User) Greet() {
 
 func (a Admin) Greet() {
 	fmt.Println("Hello", a.Name, "| you access ->", a.Access, "| you role ->", a.Role)
+}
+
+func (a Admin) ChangeAccess(access bool) {
+	a.Access = access
+	fmt.Println("Access изменён:", a.Access)
+}
+
+func (a *Admin) ChangeRole(role string) {
+	a.Role = role
+	fmt.Println("Role изменён:", a.Role)
 }
 
 func main() {
